78eb6b93bf8197a4795840fc7f7acd70b0da070c
stepan.sinitsa <stepansinitsa@gmail.com>
2024-11-01 10:58:37 +0700

    init

--- react22/src/components/CardsView.jsx
+++ react22/src/components/CardsView.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import PropTypes from 'prop-types'
 import ShopCard from './ShopCard'
 import uniqid from 'uniqid'
--- react22/src/components/Store.jsx
+++ react22/src/components/Store.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import PropTypes from 'prop-types';
 import CardsView from './CardsView';
 import ListView from './ListView';
